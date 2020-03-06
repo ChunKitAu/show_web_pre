de522ed715d94aa55aceff8c2f7b8175ef7388ce
libaiqi-gt <892208148@qq.com>
2020-03-06 17:30:00 +0800

    公共页面修改成用load方法引用公共页面

---
+++ js/index/commons/public_page.js
@@ -0,0 +1,7 @@
+
+    $(function () {
+        /*引用头部公共文件*/
+        $("#_header").load("index_header.html");
+        $("#_footer").load("index_footer.html");
+        $("#content_left").load("index_content_left.html");
+    })
